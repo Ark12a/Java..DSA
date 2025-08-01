b2fff7706a7daee9ff3b8ccf0ec7c37b74a1f009
Arkadyuti Dutta <duttorko69@gmail.com>
2025-08-01 16:24:56 +0000

    Modify StringBuilder

--- String_Builder.java
+++ String_Builder.java
@@ -1,9 +1,16 @@
 public class String_Build{
     public static void main(String []args){
         StringBuilder sb = new StringBuilder("Tanka");
+        System.out.println(sb);//prints StringBuilder String
+
+        System.out.println(sb.charAt(0));//prints char at 0th index
+
+        sb.setCharAt(0,'S');//set char at 0th Position
+        //Tanka ---> Sanka
         System.out.println(sb);
 
-        sb.setCharAt(0,'S');
+        sb.insert(0,'P'); //insert char at 0th Position 
+        //Sanka --->  PSanka
         System.out.println(sb);
     }
 }
