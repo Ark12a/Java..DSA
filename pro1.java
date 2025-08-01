20eec48ec4a41d26c108ff046751bcf27c2790a5
Arkadyuti Dutta <duttorko69@gmail.com>
2025-08-01 15:34:57 +0000

    Modified Program

--- pro1.java
+++ pro1.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class pro1{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        int b;
+        System.out.println("Enter a number");
+        b=sc.nextInt();
+        System.out.println("No. is"+b);
     }
 }
