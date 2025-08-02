ca2943e1ecdb878ed4b0a4f3fee1615e50ca7977
Arkadyuti Dutta <duttorko69@gmail.com>
2025-08-02 13:41:01 +0000

    Bit Manupulation

---
+++ Bit_Man.java
@@ -0,0 +1,13 @@
+public class Bit_Man{
+    public static void main(String args[]){
+        int n=5;
+        int pos=2;
+        int bm = 1<<pos;
+        if((bm & n)==0){
+            System.out.println("Bit is zero";
+        }
+        else{
+            System.out.println("Bit is one");
+        }
+    }
+}
